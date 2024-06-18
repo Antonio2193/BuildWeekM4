9c41eba5a3afc4fa36f74ef38a5ed1b06132b8b9
MauroGi24 <160973771+MauroGi24@users.noreply.github.com>
2024-06-18 20:50:07 +0200

    add js

---
+++ BuildWeekM4/assets/js/script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    ultimoBrano()
+    
+});
+
+let artista = ['Iron Maiden', 'Metallica', 'Dream Theater', 'Pink Floyd', 'Queen', 'Gianni Celeste', 'Squallor']
+let artistaRandom = (Math.round((Math.random()*artista.length)));
+let artistaIndex = artista[artistaRandom]
+
+
+function ultimoBrano(){
+    fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${artistaIndex}`)
+        .then((response)=>{
+            response.json().then((data)=>{
+            });
+        }) 
+        
+    .catch((err)=>{console.log("errore"+ err);});
+    }
+
+    
+
+
+
+
