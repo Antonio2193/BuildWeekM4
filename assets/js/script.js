b6fc920bd83c19c76b31367a0a13fd5cb030a80f
Antonio2193 <161360723+Antonio2193@users.noreply.github.com>
2024-06-19 10:57:57 +0200

    aggunta funzione buonasera

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", () => {
+    pescaArtista();
+});
+function ultimoBrano(artistaIndex) {
+    fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${artistaIndex}`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            
+        })
+        .catch((err) => {
+            console.log("errore: " + err);
+        });
+} 
+
+function pescaArtista() {
+    // Genera un numero casuale tra 1 e 150
+    const idRandom = Math.floor(Math.random() * 150) + 1;
+
+    fetch(`https://striveschool-api.herokuapp.com/api/deezer/artist/${idRandom}`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            // Passiamo il nome dell'artista alla funzione ultimoBrano
+            ultimoBrano(data.name);
+            buonasera(data.name);
+        })
+        .catch((err) => {
+            console.log("errore: " + err);
+        });
+}
+
+
+function buonasera(artistaIndex) {
+    fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${artistaIndex}`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log("buonasera");
+            console.log(data);
+            
+        })
+        .catch((err) => {
+            console.log("errore: " + err);
+        });
+
+}
